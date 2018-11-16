4be3cf9a52dc4c99c68443428babca7f9a707c92
Glaudson deJesus <Glaudson.deJesus@byg.io>
2018-11-15 20:42:01 -0800

    Remove env var from development run

--- config/local.js
+++ config/local.js
@@ -1,6 +1,6 @@
 let CONFIG = {}
 
-// Value hard code are for developemnt
+// Value hard coded are for developemnt
 // Set environment variables for production
 
 CONFIG.APP      = process.env.APP       || "url-slugger";
@@ -9,7 +9,7 @@ CONFIG.HOST     = process.env.HOST      || "http://localhost";
 CONFIG.PORT     = process.env.PORT      || "3000";
 
 CONFIG.DB_HOST  = process.env.DB_HOST   || "localhost";
-CONFIG.DB_NAME  = process.env.DB_NAME   || "project";
+CONFIG.DB_NAME  = process.env.DB_NAME   || "slugger_development";
 CONFIG.DB_USER  = process.env.DB_USER   || "dev";
 CONFIG.DB_PWD   = process.env.DB_PWD    || "";
 
