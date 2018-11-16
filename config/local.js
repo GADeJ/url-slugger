e5f6f23d03b982f4aed302d69c31f7a4363ec720
Glaudson deJesus <Glaudson.deJesus@byg.io>
2018-11-15 21:38:10 -0800

    Renamed env vars

--- config/local.js
+++ config/local.js
@@ -11,7 +11,7 @@ CONFIG.PORT     = process.env.PORT      || 3000;
 CONFIG.DB_HOST  = process.env.DB_HOST   || "localhost";
 CONFIG.DB_NAME  = process.env.DB_NAME   || "slugger_development";
 CONFIG.DB_USER  = process.env.DB_USER   || "dev";
-CONFIG.DB_PWD   = process.env.DB_PWD    || "";
+CONFIG.DB_PASS  = process.env.DB_PASS    || "";
 
 // Node Id for worker to generate unique slugs if more than 1 container
 CONFIG.NUID     = process.env.NUID      || 0;
--- database/mysql.js
+++ database/mysql.js
@@ -6,7 +6,7 @@ var connection = mysql.createPool({
     host:       CONFIG.DB_HOST,
     database:   CONFIG.DB_NAME,
     user:       CONFIG.DB_USER,
-    password:   CONFIG.DB_PWD
+    password:   CONFIG.DB_PASS
 });
 
 module.exports = connection;
