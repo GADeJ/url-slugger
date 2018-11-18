9f83ad6cf9139c6389a048a4eb2f803bff043ef9
Glaudson deJesus <Glaudson.deJesus@byg.io>
2018-11-18 02:32:36 -0800

    Handling database creation for container

--- documentation/schema.sql
+++ documentation/schema.sql
@@ -15,6 +15,15 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+--
+-- Database `slugger`
+--
+
+CREATE DATABASE slugger
+USE slugger
+
+
 --
 -- Table structure for table `slugger`
 --
@@ -29,19 +38,10 @@ CREATE TABLE `slugger` (
   `custom` tinyint(1) DEFAULT '0',
   `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `slugger`
---
-
-LOCK TABLES `slugger` WRITE;
-/*!40000 ALTER TABLE `slugger` DISABLE KEYS */;
-INSERT INTO `slugger` VALUES (1,'w$qe92Ejg','https://www.google.com',0,'2018-11-16 05:59:08'),(2,'qV50y0gZk','https://www.reddit.com',0,'2018-11-16 05:59:42'),(3,'CUSTOM1','https://www.reddit.com',1,'2018-11-16 06:00:02'),(5,'CUSTOM2','https://www.reddit.com',1,'2018-11-16 06:00:08'),(6,'CUSTOM3','https://www.reddit.com',1,'2018-11-16 06:00:12'),(7,'CUSTOM4','https://www.reddit.com',1,'2018-11-16 06:00:16'),(8,'CUSTOM5','https://www.reddit.com',1,'2018-11-16 06:00:19'),(9,'UKPxQDDNW','https://www.twitter.com',0,'2018-11-16 06:00:31'),(10,'Whickfomc','https://www.instagram.com',0,'2018-11-16 10:57:47'),(11,'CUSTOM6','https://www.instagram.com',1,'2018-11-16 10:59:13'),(13,'custom1','https://www.instagram.com',1,'2018-11-16 11:46:26');
-/*!40000 ALTER TABLE `slugger` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `visit`
@@ -61,16 +61,6 @@ CREATE TABLE `visit` (
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `visit`
---
-
-LOCK TABLES `visit` WRITE;
-/*!40000 ALTER TABLE `visit` DISABLE KEYS */;
-INSERT INTO `visit` VALUES (1,1,'172.30.0.213','2018-11-16 10:57:15'),(2,1,'172.30.0.213','2018-11-16 11:06:15'),(3,3,'172.30.0.213','2018-11-16 11:09:49'),(4,3,'172.30.0.213','2018-11-16 11:10:40'),(5,3,'172.30.0.213','2018-11-16 11:10:51'),(6,11,'172.30.0.213','2018-11-16 11:14:35'),(7,11,'172.30.0.213','2018-11-16 11:15:32'),(8,11,'172.30.0.213','2018-11-16 11:20:10'),(9,11,'172.30.0.213','2018-11-16 11:20:14'),(10,11,'172.30.0.213','2018-11-16 11:20:17'),(11,11,'172.30.0.213','2018-11-16 11:24:48'),(12,3,'172.30.0.213','2018-11-16 11:31:47'),(13,3,'172.30.0.213','2018-11-16 11:31:54'),(14,3,'172.30.0.213','2018-11-16 11:34:52'),(15,3,'172.30.0.213','2018-11-16 11:35:21'),(16,3,'172.30.0.213','2018-11-16 11:37:46');
-/*!40000 ALTER TABLE `visit` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
