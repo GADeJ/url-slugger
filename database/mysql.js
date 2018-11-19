6350468997238c2d9efa78cc8c864e35ee4f3cc7
Glaudson deJesus <Glaudson.deJesus@byg.io>
2018-11-19 02:38:12 -0800

    Database configuration instruction

--- database/mysql.js
+++ database/mysql.js
@@ -2,6 +2,7 @@ const CONFIG = require("../config/local");
 
 var mysql = require("mysql");
 
+// Password so be set in an environment variable
 var connection = mysql.createPool({
     host:       CONFIG.DB_HOST,
     database:   CONFIG.DB_NAME,
